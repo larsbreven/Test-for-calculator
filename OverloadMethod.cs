19f27302e3562cf53e349efda740036f9cf295f9
Lars Brevén <lars.breven@telia.com>
2021-03-03 20:54:54 +0100

    Addition and overload

---
+++ OverloadMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_for_calculator
+{
+    public class OverloadMethod
+    {
+        public double Addition(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+
+    }
+
+}
+
+
